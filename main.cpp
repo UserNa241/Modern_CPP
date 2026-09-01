e2d2c818c4c6a5230dfed6c68f816786dfc98a25
userNa241 <185086099+UserNa241@users.noreply.github.com>
2026-09-01 19:23:26 +0545

    Add 2.2_Ruleof5.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <stdexcept>
+#include <cstddef>
+#include <chrono>
+
+class TimerGuard {
+public:
+	explicit TimerGuard (const char* data) : data_(new char[std::strlen(data)+1]) {
+		std::strcpy(data_,data);
+		now = std::chrono::steady_clock::now();
+	};
+
+	~TimerGuard() noexcept {
+		end = std::chrono::steady_clock::now();
+		std::cout << data_ << " took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - now) << '\n';
+		delete[] data_;
+	}
+
+	TimerGuard (const TimerGuard&) = delete;
+	TimerGuard operator=(const TimerGuard&) = delete;
+
+private:
+	char *data_;
+	std::chrono::time_point<std::chrono::steady_clock>now, end;
+};
 
 int main() {
-	int* p = new int[5];
-
-	volatile int index = 5;
-	p[index] = 42;
-
-	std::cout << p[index] << '\n';
-
-	delete[] p;
+	TimerGuard T("Arena");
+	for (int i = 0; i < 10000000; i++) {}
+	return 0;
 }
